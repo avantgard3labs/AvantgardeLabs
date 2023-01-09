c6315348c5ae6b6f7d2ba65dde7fcc7eff2e1dea
ssiddarthsai1999 <ssiddarthsai1999@gmail.com>
2023-01-09 15:56:21 +0530

    updatedConfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+};
+module.exports = {
+    images: {
+        loader: "akamai",
+        path: "",
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
